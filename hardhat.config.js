fc1d67c36ef37d6fa827e6bb9649c959a1a81c60
sarahsadounn <sarahnuodas@gmail.com>
2025-04-22 22:52:36 +0100

    test with sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config(); // Optional (for environment variables)
+require("dotenv").config();
 
 module.exports = {
-    solidity: "0.8.0",
     networks: {
-        // Local network (for testing)
-        hardhat: {},
-
-        // Goerli Testnet (example)
-        goerli: {
-            url: process.env.ALCHEMY_GOERLI_URL, //environment variables
-            accounts: [process.env.PRIVATE_KEY], // Wallet private key
+        sepolia: {
+            url: process.env.ALCHEMY_SEPOLIA_URL,
+            accounts: [process.env.PRIVATE_KEY],
         },
-
-        // Add other networks (e.g., Mainnet) as needed
-    }
+    },
 };
